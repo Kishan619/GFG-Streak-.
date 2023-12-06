42bf57fed47f3b728cd7b16b7e79917b0243a230
Kishan Kumar Rauniyar <91432314+Kishan619@users.noreply.github.com>
2023-12-06 11:52:01 +0530

    Create How Many X's?.cpp

---
+++ How Many X's?.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int countX(int L, int R, int X) {
+        // code here
+        int c=0;
+        for(int i=L+1;i<R;i++){
+            int t=i;
+            while(t>0){
+                int rem=t%10;
+                if(rem==X)
+                c++;
+                t=t/10;
+            }
+        }
+        return c;
+    }
+};
